bb2d864bf191cad8359ad70cd18be41673f13c69
kev <fbian.kevin@gmail.com>
2025-06-02 10:50:01 +0800

    Initialize Microweb Spring Boot Starter module
    - Added essential dependencies: Spring Boot Actuator, Spring Web, Spring Validation, Spring Doc, Logbook, DevTools, and Lombok.
    - Created `pom.xml` with parent POM reference and custom project properties.
    - Configured `.gitignore` for common IDEs and build tools.
    - Added `HELP.md` with references and guides for getting started.
    - Updated `MicrowebAutoConfiguration` to load properties from `application.yaml`.

--- microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/config/MicrowebAutoConfiguration.java
+++ microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/config/MicrowebAutoConfiguration.java
@@ -1,7 +1,9 @@
 package com.github.fabiankevin.microwebspringbootstarter.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@PropertySource("classpath:application.yaml")
 public class MicrowebAutoConfiguration {
 }
