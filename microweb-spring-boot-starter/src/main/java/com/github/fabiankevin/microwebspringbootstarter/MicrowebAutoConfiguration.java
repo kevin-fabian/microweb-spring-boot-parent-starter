077adf4807daa80ac3ee4cc296398c929f104baf
kev <fbian.kevin@gmail.com>
2025-06-03 18:34:53 +0800

    Add conditional bean creation for GlobalExceptionHandler and update configuration files

--- microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/MicrowebAutoConfiguration.java
+++ microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/MicrowebAutoConfiguration.java
@@ -1,6 +1,7 @@
 package com.github.fabiankevin.microwebspringbootstarter;
 
 import com.github.fabiankevin.microwebspringbootstarter.web.GlobalExceptionHandler;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
@@ -10,6 +11,7 @@ import org.springframework.context.annotation.PropertySource;
 public class MicrowebAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public GlobalExceptionHandler globalExceptionHandler() {
         return new GlobalExceptionHandler();
     }
