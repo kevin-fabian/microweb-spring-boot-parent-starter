888367dda5cbd6e5275a338b8eb5e9d86ad7f6cf
kev <fbian.kevin@gmail.com>
2025-06-07 10:45:55 +0800

    Bump version to 0.0.23-SNAPSHOT and add application-microweb.properties for configuration

--- microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/MicrowebAutoConfiguration.java
+++ microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/MicrowebAutoConfiguration.java
@@ -2,14 +2,14 @@ package com.github.fabiankevin.microwebspringbootstarter;
 
 import com.github.fabiankevin.microwebspringbootstarter.web.GlobalExceptionHandler;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 
 @AutoConfiguration
+@PropertySource("classpath:application-microweb.properties")
 public class MicrowebAutoConfiguration {
 
     @Bean
-    @ConditionalOnMissingBean
     public GlobalExceptionHandler globalExceptionHandler() {
         return new GlobalExceptionHandler();
     }
