b533f9d33f921a2b37cbcadfbde6f49ea8070a4f
kev <fbian.kevin@gmail.com>
2025-06-07 08:25:57 +0800

    Update pom.xml for version bump and remove unused dependencies; refactor MicrowebAutoConfiguration to use @AutoConfiguration

--- microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/MicrowebAutoConfiguration.java
+++ microweb-spring-boot-starter/src/main/java/com/github/fabiankevin/microwebspringbootstarter/MicrowebAutoConfiguration.java
@@ -1,13 +1,11 @@
 package com.github.fabiankevin.microwebspringbootstarter;
 
 import com.github.fabiankevin.microwebspringbootstarter.web.GlobalExceptionHandler;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 
-@Configuration
-@PropertySource("classpath:application.yaml")
+@AutoConfiguration
 public class MicrowebAutoConfiguration {
 
     @Bean
